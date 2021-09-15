74df04dd895f8d919b78d94ec30cf6bb6ae18d2d
Adithya Vijay K <adithyavj@gmail.com>
2021-09-15 17:20:14 +0530

    Call get, getbyid, insert methods in consoleUI

--- SQLServerUI/Program.cs
+++ SQLServerUI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
 
 namespace SQLServerUI
 {
@@ -8,7 +10,50 @@ namespace SQLServerUI
     {
         static void Main(string[] args)
         {
+            SqlCrud sql = new SqlCrud(GetConnectionString());
+            //ReadAllContacts(sql);
+            //ReadContact(sql, 1);
+            CreateNewContact(sql);            
+
             Console.ReadLine();
+        }
+
+        private static void CreateNewContact(SqlCrud sql)
+        {
+            FullContactModel user = new FullContactModel
+            {
+                BasicInfo = new BasicContactModel
+                {
+                    FirstName = "Athulya",
+                    LastName = "Vijay"
+                },
+            };
+
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "athulyavj@gmail.com" });
+            user.EmailAddresses.Add(new EmailAddressModel { Id = 3, EmailAddress = "info@adithyavj.in" });
+
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "1234656789" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { Id = 2, PhoneNumber = "9946173471" });
+
+            sql.CreateContact(user);
+
+        }
+
+        private static void ReadAllContacts(SqlCrud sql)
+        {
+            var rows = sql.GetAllContacts();
+            foreach (var row in rows)
+            {
+                Console.WriteLine($"{row.Id} {row.FirstName} {row.LastName}");
+            }
+        }
+
+        private static void ReadContact(SqlCrud sql,int contactId)
+        {
+            var contact = sql.GetFullContactById(contactId);
+
+            Console.WriteLine($"{contact.BasicInfo.Id} {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
+            
         }
 
         private static string GetConnectionString(string connectionStringName = "Default")
