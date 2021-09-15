0d42550a596babe65a0d348e878e1e133ca6816d
Adithya Vijay K <adithyavj@gmail.com>
2021-09-15 14:51:19 +0530

    Add connectionstring in ConsoleUI of sql server.
    - Add connection string in appsettings.json
    - Call using config in program.cs

--- SQLServerUI/Program.cs
+++ SQLServerUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace SQLServerUI
 {
@@ -8,5 +10,21 @@ namespace SQLServerUI
         {
             Console.ReadLine();
         }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+        
     }
 }
