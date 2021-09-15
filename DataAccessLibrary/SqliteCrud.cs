594ab66aca919dbb43ed9219c3088cd5e20bb222
Adithya Vijay K <adithyavj@gmail.com>
2021-09-15 19:45:50 +0530

    Add Sqlite data access and crud.

---
+++ DataAccessLibrary/SqliteCrud.cs
@@ -0,0 +1,146 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqliteCrud
+    {
+        private readonly string _connectionString;
+        private SqliteDataAccess db = new SqliteDataAccess();
+
+        public SqliteCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<BasicContactModel> GetAllContacts()
+        {
+            string sql = "select Id, FirstName, LastName from Contacts";
+
+            return db.LoadData<BasicContactModel, dynamic>(sql, new { }, _connectionString);
+        }
+
+        public FullContactModel GetFullContactById(int id)
+        {
+            string sql = "select Id,FirstName,LastName from Contacts where Id = @Id";
+
+            FullContactModel output = new FullContactModel();
+
+            output.BasicInfo = db.LoadData<BasicContactModel, dynamic>(sql, new { Id = id }, _connectionString).FirstOrDefault();
+
+            if (output.BasicInfo == null)
+            {
+                // do something to tell the user that the record was not found.
+                // throw new Exception("User not found");
+                return null;
+            }
+
+            sql = @"select E.* 
+                    from EmailAddresses E
+                    inner join ContactEmailMapping CE on CE.EmailAddressId = E.Id
+                    where CE.ContactId = @Id";
+
+            output.EmailAddresses = db.LoadData<EmailAddressModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+            sql = @"select P.* 
+                    from PhoneNumbers P
+                    inner join ContactPhoneNumbersMapping CP on CP.PhoneNumberId = P.Id
+                    where CP.ContactId = @Id";
+
+            output.PhoneNumbers = db.LoadData<PhoneNumberModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+            return output;
+        }
+
+        public void CreateContact(FullContactModel contact)
+        {
+            // save basic contact
+            #region Insert BasicContact Details
+            string sql = "insert into Contacts(FirstName, LastName) values (@FirstName, @LastName)";
+            db.SaveData(
+                sql,
+                new { contact.BasicInfo.FirstName, contact.BasicInfo.LastName },
+                _connectionString);
+
+            // get the ID number of the created contact
+            sql = "select Id from Contacts where FirstName = @FirstName and LastName = @LastName";
+            int contactId = db.LoadData<IdLookupModel, dynamic>(
+                sql,
+                new { contact.BasicInfo.FirstName, contact.BasicInfo.LastName },
+                _connectionString).First().Id;
+            #endregion
+
+            // check if email address already exists, and insert to emailAddress table and mapping table
+            #region Insert EmailAddress and its mapping
+            foreach (var emailAddress in contact.EmailAddresses)
+            {
+                if (emailAddress.Id == 0)
+                {
+                    // insert new emailId to the table
+                    sql = "insert into EmailAddresses (EmailAddress) values (@EmailAddress)";
+                    db.SaveData(sql, new { emailAddress.EmailAddress }, _connectionString);
+
+                    // select id of inserted EmailId
+                    sql = "select Id from EmailAddresses where EmailAddress = @EmailAddress";
+                    emailAddress.Id = db.LoadData<IdLookupModel, dynamic>(
+                        sql,
+                        new { emailAddress.EmailAddress },
+                        _connectionString).First().Id;
+                }
+                // insert to mapping table
+                sql = "insert into ContactEmailMapping (ContactId, EmailAddressId) values (@ContactId, @EmailAddressId)";
+                db.SaveData(sql, new { ContactId = contactId, EmailAddressId = emailAddress.Id }, _connectionString);
+            }
+            #endregion
+
+            #region Insert PhoneNumbers and its mapping
+            // check if phone number already exists, and insert to phoneNumber table and mapping table
+            foreach (var phoneNumber in contact.PhoneNumbers)
+            {
+                if (phoneNumber.Id == 0)
+                {
+                    sql = "insert into PhoneNumbers (PhoneNumber) values (@PhoneNumber)";
+                    db.SaveData(sql, new { phoneNumber.PhoneNumber }, _connectionString);
+
+                    sql = "select Id from PhoneNumbers where PhoneNumber = @PhoneNumber";
+                    phoneNumber.Id = db.LoadData<IdLookupModel, dynamic>(sql,
+                                                                         new { phoneNumber.PhoneNumber },
+                                                                         _connectionString).First().Id;
+                }
+
+                sql = "insert into ContactPhoneNumbersMapping (ContactId, PhoneNumberId) values (@ContactId, @PhoneNumberId)";
+                db.SaveData(sql, new { ContactId = contactId, PhoneNumberId = phoneNumber.Id }, _connectionString);
+            }
+            #endregion
+        }
+
+        public void UpdateContactName(BasicContactModel contact)
+        {
+            string sql = "update Contacts set FirstName = @FirstName, LastName = @LastName where Id = @Id";
+            db.SaveData(sql, contact, _connectionString);
+        }
+
+        public void RemovePhoneNumberFromContact(int contactId, int phoneNumberId)
+        {
+            // find all usages of the phoneNumber
+            // if one then, delete link and the phoneNumber
+            // if > one then, delete link
+
+            string sql = "select Id, ContactId, PhoneNumberId from ContactPhoneNumbersMapping where PhoneNumberId = @PhoneNumberId";
+            var links = db.LoadData<ContactPhoneNumberModel, dynamic>(sql, new { PhoneNumberId = phoneNumberId }, _connectionString);
+
+            sql = "delete from ContactPhoneNumbersMapping where ContactId = @ContactId and PhoneNumberId = @PhoneNumberId";
+            db.SaveData(sql, new { ContactId = contactId, PhoneNumberId = phoneNumberId }, _connectionString);
+
+            if (links.Count == 1)
+            {
+                sql = "delete from PhoneNumbers where Id = @Id";
+                db.SaveData(sql, new { Id = phoneNumberId }, _connectionString);
+            }
+        }
+    }
+}
--- SqliteUI/Program.cs
+++ SqliteUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 
@@ -8,15 +10,69 @@ namespace SqliteUI
     {
         static void Main(string[] args)
         {
-            //SqlCrud sql = new SqlCrud(GetConnectionString());
+            SqliteCrud sql = new SqliteCrud(GetConnectionString());
             //ReadAllContacts(sql);
-            //ReadContact(sql, 1);
+            //ReadContact(sql, 2);
             //CreateNewContact(sql);            
             //UpdateContact(sql);
             //RemoveContact(sql, 1, 1);
 
             Console.WriteLine("Done processing Sqlite!");
             Console.ReadLine();
+        }
+
+        private static void RemoveContact(SqliteCrud sql, int contactId, int phoneNumberId)
+        {
+            sql.RemovePhoneNumberFromContact(contactId, phoneNumberId);
+        }
+
+        private static void UpdateContact(SqliteCrud sql)
+        {
+            BasicContactModel user = new BasicContactModel
+            {
+                Id = 1,
+                FirstName = "Adithya",
+                LastName = "Vijay K"
+            };
+            sql.UpdateContactName(user);
+        }
+
+        private static void CreateNewContact(SqliteCrud sql)
+        {
+            FullContactModel user = new FullContactModel
+            {
+                BasicInfo = new BasicContactModel
+                {
+                    FirstName = "Athulya",
+                    LastName = "Vijay"
+                },
+            };
+
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "athulyavj@gmail.com" });
+            user.EmailAddresses.Add(new EmailAddressModel { Id = 2, EmailAddress = "info@adithyavj.in" });
+
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "1234656789" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { Id = 2, PhoneNumber = "9946173471" });
+
+            sql.CreateContact(user);
+
+        }
+
+        private static void ReadAllContacts(SqliteCrud sql)
+        {
+            var rows = sql.GetAllContacts();
+            foreach (var row in rows)
+            {
+                Console.WriteLine($"{row.Id} {row.FirstName} {row.LastName}");
+            }
+        }
+
+        private static void ReadContact(SqliteCrud sql, int contactId)
+        {
+            var contact = sql.GetFullContactById(contactId);
+
+            Console.WriteLine($"{contact.BasicInfo.Id} {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
+
         }
 
         private static string GetConnectionString(string connectionStringName = "Default")
