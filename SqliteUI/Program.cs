b1577a1089f1767f53a56e51ec11145c914345d5
Adithya Vijay K <adithyavj@gmail.com>
2021-09-15 19:21:34 +0530

    Added Sqlite UI project, configured connection string.

---
+++ SqliteUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace SqliteUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SqlCrud sql = new SqlCrud(GetConnectionString());
+            //ReadAllContacts(sql);
+            //ReadContact(sql, 1);
+            //CreateNewContact(sql);            
+            //UpdateContact(sql);
+            //RemoveContact(sql, 1, 1);
+
+            Console.WriteLine("Done processing Sqlite!");
+            Console.ReadLine();
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+    }
+}
